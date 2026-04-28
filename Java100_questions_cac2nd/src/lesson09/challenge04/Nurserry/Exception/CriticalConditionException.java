6ac3814cc54124e56440a5d5b768b96619a2b089
htfv-tech <zaqzaqhahaha@gmail.com>
2026-04-28 10:37:00 +0900

    Create CriticalConditionException.java完了しました。

---
+++ Java100_questions_cac2nd/src/lesson09/challenge04/Nurserry/Exception/CriticalConditionException.java
@@ -0,0 +1,10 @@
+package lesson09.challenge04.Nurserry.Exception;
+
+public class CriticalConditionException extends Exception {
+	/**
+	 * @param msg
+	 */
+	public CriticalConditionException(String msg) {
+		super(msg);
+	}
+}
